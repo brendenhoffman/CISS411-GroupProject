e15e487d6aba500e4c6bb66ede9be6ac298e4d98
Allan <aclopezsandoval1@cougars.ccis.edu>
2025-10-01 17:16:50 -0700

    User Story 12

---
+++ Models/NotificationsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CISS411_GroupProject.Models;
+using CISS411_GroupProject.Data;
+using System;
+using System.Linq;
+
+namespace CISS411_GroupProject.Models
+{
+    public class NotificationsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public NotificationsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult NewRegistrations()
+        {
+            {
+                // Get users registered in last 24 hours
+                var newUsers = _context.Users
+                    .Where(u => u.CreatedAt >= DateTime.Now.AddDays(-1) && u.Role =="Visitor" && u.Status == "Pending Confirmation")
+                    .Select(u => new { u.FirstName, u.LastName, u.Email })
+                    .ToList();
+
+                return Json(newUsers);
+            }
+        }
+    }
+}
